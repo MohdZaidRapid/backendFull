3de45afc155d41a42c06a77671176854df5ea90e
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-11-07 02:10:09 +0530

    added error handler

--- src/index.js
+++ src/index.js
@@ -6,27 +6,12 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
-/*
-import express from "express";
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("Error", error);
-      throw error;
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
     });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`app is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR", error);
-    throw error;
-  }
-})();
-
-*/
+  })
+  .catch((err) => {
+    console.log("MONGODB db connect failed !!!", err);
+  });
