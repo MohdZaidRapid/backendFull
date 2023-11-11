6787936a12d26408c2b651edd42b89fbed7befd5
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-11-11 21:09:06 +0530

    add models finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+// import express from "express";
+// const app = express();
 
 dotenv.config({
   path: "./env",
