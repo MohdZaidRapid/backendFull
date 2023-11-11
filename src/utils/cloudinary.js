287ac391e7ef32d2ea822d16396d62967a91cdc9
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-11-11 22:09:37 +0530

    setup complete

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localfilePath) => {
+  try {
+    if (!localfilePath) return null;
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localfilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfull
+    console.log("file is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localfilePath); //remove the malicious locally save temporary file as the upload operation got fail
+  }
+};
+
+cloudinary.v2.uploader.upload(
+  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) {
+    console.log(result);
+  }
+);
+
+export { uploadOnCloudinary };
