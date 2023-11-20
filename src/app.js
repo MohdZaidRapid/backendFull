cdceb49a3ccdb1f24bf65f06a090ad5fdf88185f
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-11-20 23:00:55 +0530

    all setup configure

--- src/app.js
+++ src/app.js
@@ -18,6 +18,13 @@ app.use(
 );
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser ());
+app.use(cookieParser());
+
+// routes
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
 
 export { app };
