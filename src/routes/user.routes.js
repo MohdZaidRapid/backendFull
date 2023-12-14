37c697e3061fa625a92dc960b4cffd5db57dd3cc
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-12-14 19:42:33 +0530

    added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -18,5 +23,10 @@ router.route("/register").post(
   ]),
   registerUser
 );
+
+router.route("/login").post(loginUser);
+
+// Secure routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
